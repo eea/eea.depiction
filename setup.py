96f7c0e6df360776b9a2c333653f73efd77ddd94
pthulin <pthulin@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-11-12 15:40:22 +0000

    Read version from version.txt file. This avoids having to change it on two locations for Plone and setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 from setuptools import setup, find_packages
 import os
+from os.path import join
 
-version = '0.1'
+name = 'valentine.imagescales'
+path = name.split('.') + ['version.txt']
+version = open(join(*path)).read().strip()
 
 setup(name='valentine.imagescales',
       version=version,
