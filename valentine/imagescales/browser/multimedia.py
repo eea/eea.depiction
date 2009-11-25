867201c62130b766ef5f074b50ad5aa24fb85f51
pthulin <pthulin@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-11-25 15:32:55 +0000

    Fixed bug where 16:9 images scaled to 16:9 background resulted in black borders

--- valentine/imagescales/browser/multimedia.py
+++ valentine/imagescales/browser/multimedia.py
@@ -67,14 +67,27 @@ def thumbnail(orig, button, size):
     # Create an image with the requested size
     bg = PIL.Image.new('RGB', size, color=(0, 0, 0))
 
-    # Scale the original image. If we're scaling a 4:3 to 16:9, we max the
-    # height and scale the width as far as we can, and vice versa
-    if ((float(size[0])/size[1]) > (float(orig.size[0])/orig.size[1])):
-        aspect = float((size[1]) / float(orig.size[1]))
-        thumb_size = (int(size[0] * aspect), orig.size[1])
+    # Investigate the dimensions of the resulting image depending on if we
+    # scale the width or the height
+    scale = float(size[0]) / orig.size[0]
+    scaled_height = int(scale * orig.size[1])
+
+    scale = float(size[1]) / orig.size[1]
+    scaled_width = int(scale * orig.size[0])
+
+    # If there's less than 10 px between the scaled version and the resulting
+    # size, we clamp the image (hope no-one will see the difference!). This
+    # is to avoid tiny black borders
+    if (size[0] - scaled_width) < 10:
+        scaled_width = size[0]
+    if (size[1] - scaled_height) < 10:
+        scaled_height = size[1]
+
+    # Scale in the direction that results in least black borders
+    if (scaled_height - size[1]) < (scaled_width - size[0]):
+        thumb_size = (size[0], scaled_height)
     else:
-        aspect = float((size[0]) / float(orig.size[0]))
-        thumb_size = (size[0], int(orig.size[1] * aspect))
+        thumb_size = (scaled_width, size[1])
     thumb = orig.resize(thumb_size, PIL.Image.ANTIALIAS)
 
     # Paste it onto the background (centered)
@@ -85,7 +98,7 @@ def thumbnail(orig, button, size):
     bg.paste(thumb, (x, y, w, h))
 
     # Apply the play button (centered)
-    scale = float(size[1]) / button.size[1]
+    scale = float(thumb_size[1]) / button.size[1]
     new_button_size = (int(button.size[0]*scale), int(button.size[1]*scale))
     button = button.resize(new_button_size, PIL.Image.ANTIALIAS)
     x = (bg.size[0]/2) - (button.size[0]/2)
