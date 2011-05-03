40e5690ff27a0893d1e75b82a6b704fe3bfce4b4
afd <afd@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-05-03 15:48:04 +0000

    Prepare valentine.imagescales 0.8.

--- valentine/imagescales/browser/atfolder.py
+++ valentine/imagescales/browser/atfolder.py
@@ -6,28 +6,36 @@ from interfaces import IImageView
 
 class ImageView(BrowserView):
 
-    """This view takes the first published/visible image found in a folder and returns it in
-    the requested size."""
+    """This view takes the first published/visible image found in a folder
+    and returns it in the requested size."""
 
     implements(IImageView)
 
-    def __init__(self, context, request):
-        super(ImageView, self).__init__(context, request)
-        results = context.getFolderContents(contentFilter={
-            'portal_type': 'Image',
-            'review_state': ['published','visible'],
-        })
+    img = None
+
+    def display(self, scalename='thumb'):
+        """return a bool if the scale should be displayed
+        """
+        here = '/'.join(self.context.getPhysicalPath())
+        results = self.context.portal_catalog.queryCatalog(
+                {
+                    'portal_type':'Image',
+                    'path': {
+                        'query':here,
+                        'depth':1,
+                        },
+                    }, #show_all=1, show_inactive=1,
+                )
         self.field = None
         self.has_images = False
         if len(results) > 0:
             self.has_images = True
             self.img = results[0].getObject()
             self.field = self.img.getField('image')
-
-    def display(self, scalename='thumb'):
         if not self.has_images:
             return False
-        return (self.field != None) and bool(self.field.getScale(self.img, scalename))
+        return (self.field != None) and \
+                    bool(self.field.getScale(self.img, scalename))
 
     def __call__(self, scalename='thumb'):
         if not self.display(scalename):
