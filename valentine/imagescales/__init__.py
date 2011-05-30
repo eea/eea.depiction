de612f8b4c77b2ae6a6940c1288b59f5f0755958
alecghica <alecghica@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-05-30 11:44:57 +0000

    Cleanup old installer Updated new installer

--- valentine/imagescales/__init__.py
+++ valentine/imagescales/__init__.py
@@ -1,18 +1,5 @@
 """ Valentine image scales package
 """
-#from os.path import dirname
-#from Globals import package_home
-#from Products.CMFCore import utils as cmfutils
-#from Products.CMFCore.DirectoryView import registerDirectory
-
-#ppath = cmfutils.ProductsPath
-#cmfutils.ProductsPath.append(dirname(package_home(globals())))
-#cmfutils.ProductsPath = ppath
-
 #TODO: plone4, I've disabled the patch because this should be
 #        covered by now by default plone
 #import patches
-
-#def initialize(context):
-    #"""Initializer called when used as a Zope 2 product."""
-
