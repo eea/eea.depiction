9e668bfeb572add69c5115a48c1162d0d9fd0337
avoinea <avoinea@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-11-15 15:42:46 +0000

    [JENKINS] - Fixed pylint warnings.

--- valentine/imagescales/browser/default.py
+++ valentine/imagescales/browser/default.py
@@ -13,41 +13,59 @@ from zope.publisher.interfaces.browser import IBrowserPublisher
 
 
 class Base(object):
+    """ Base
+    """
     name = None
 
     def __call__(self):
-        adapter = getMultiAdapter((self.context, self.request), IBrowserPublisher)
+        adapter = getMultiAdapter((
+            self.context, self.request), IBrowserPublisher)
         scale = adapter.publishTraverse(self.request, self.name)
         return scale.index_html(self.request, self.request.response)
 
     def absolute_url(self):
+        """ URL
+        """
         return self.context.absolute_url() + '/' + self.name
 
 
 class ImageLarge(Base):
+    """ Large
+    """
     name = "image_large"
 
 
 class ImagePreview(Base):
+    """ Preview
+    """
     name = "image_preview"
 
 
 class ImageMini(Base):
+    """ Mini
+    """
     name = "image_mini"
 
 
 class ImageThumb(Base):
+    """ Thumb
+    """
     name = "image_thumb"
 
 
 class ImageTile(Base):
+    """ Title
+    """
     name = "image_tile"
 
 
 class ImageIcon(Base):
+    """ Icon
+    """
     name = "image_icon"
 
 
 class ImageListing(Base):
+    """ Listing
+    """
     name = "image_listing"
-
