da30b657c93510469e260b848a53f853bee84278
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2017-10-25 15:20:35 +0300

    Refs #89405 CCA; avoid error when dexterity content has no image field

--- eea/depiction/browser/dexterity.py
+++ eea/depiction/browser/dexterity.py
@@ -1,10 +1,11 @@
 """ Dexterity
 """
+
+from eea.depiction.browser.interfaces import IImageView
+from Products.Five.browser import BrowserView
+from zope.component import queryMultiAdapter
 from zope.interface import implements
 from zope.publisher.interfaces import NotFound
-from zope.component import queryMultiAdapter
-from Products.Five.browser import BrowserView
-from eea.depiction.browser.interfaces import IImageView
 
 
 class DexterityImageView(BrowserView):
@@ -15,16 +16,17 @@ class DexterityImageView(BrowserView):
 
     def __init__(self, context, request):
         super(DexterityImageView, self).__init__(context, request)
-        self.field = getattr(context, 'image')
+        self.field = getattr(context, 'image', None)
 
     def display(self, scalename='thumb'):
         """ Display
         """
+
         if not bool(self.field):
             return False
 
         scaleview = queryMultiAdapter((self.context, self.request),
-            name='images')
+                                      name='images')
         scale = scaleview.scale('image', scale=scalename)
 
         if not scale:
@@ -37,7 +39,7 @@ class DexterityImageView(BrowserView):
             raise NotFound(self.request, scalename)
 
         scaleview = queryMultiAdapter((self.context, self.request),
-            name='images')
+                                      name='images')
         scale = scaleview.scale('image', scale=scalename)
 
         if scale:
